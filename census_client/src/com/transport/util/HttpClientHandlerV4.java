5d73204eb8bff77d98416bc8ab453f8f4bc09ccd
admin <admin@admin-PC>
2017-04-03 00:10:44 +0530

    structure for httpClient 4 has done.

--- census_client/src/com/transport/util/HttpClientHandlerV4.java
+++
@@ -1,8 +0,0 @@
-package com.transport.util;
-
-import java.io.IOException;
-
-public interface HttpClientHandlerV4 {
-	public void initialize();
-	public String doGet(String url) throws IOException;
-}
