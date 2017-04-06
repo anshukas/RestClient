05036be373b73e134f39ea438c758cf63da57f29
anshukas <anshubtechinit@gmail.com>
2017-04-06 12:47:25 +0530

    testing for git ignore working pt2.

--- census_client_httpclient-3.1/src/com/transport/driver/DriverClass.java
+++ census_client_httpclient-3.1/src/com/transport/driver/DriverClass.java
@@ -20,7 +20,8 @@ public class DriverClass {
 		
 		Properties pro = dc.readDataFromPropertiesFile("config.properties");
 		
-		// Transport layer or GET call
+		// Transport layer or GET call.
+		
 		HttpClientHandlerV3 httpClientHandler = new HttpClientHandlerImpl();
 		httpClientHandler.initialize();
 		// Get request call with zipcode 00602 which is hard code now in properties file. 
